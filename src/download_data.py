649f1629e7a7eb954555e12d2fa65e27c1c39d15
jo4356 <joyce_tzu_chia_wang@yahoo.ca>
2021-11-18 17:31:46 -0800

    updated download script

--- src/download_data.py
+++ src/download_data.py
@@ -6,19 +6,19 @@
 Usage: download_data.py --url=<url> --file_path=<file_path>
 
 Options:
---url=<url>                  The url of the data file to download. Must be a csv file.
---file_path=<file_path>      File path (including file name with .csv extension) to store the file
+--url=<url>                  The url of the data file to download.
+--file_path=<file_path>      File path (including file name with extension) to store the file
 """
 
-import pandas as pd
+import requests
 from docopt import docopt
 
 opt = docopt(__doc__)
 
 def main(url, file_path):
-    data = pd.read_csv(url)
-    open(file_path, 'w')
-    data.to_csv(file_path)
+    r = requests.get(url)
+    open(file_path, "wb").write(r.content)
+
 
 if __name__ == "__main__":
     main(opt["--url"], opt["--file_path"])
