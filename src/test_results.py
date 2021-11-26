16c331b9c59f22cd47df286699e6c00d8484c75a
Wenxin Xiang <xiangwx@student.ubc.ca>
2021-11-26 11:42:23 -0800

    Finish first draft

--- src/test_results.py
+++ src/test_results.py
@@ -1,4 +1,4 @@
-# author: Wenxin Xiang
+# author: Wenxin Xiang, Joyce Wang, Amelia Tang, Artan Zandian
 # date: 2021-11-26
 
 """Test selected model's accuracy on test data set.
@@ -17,27 +17,30 @@ import string
 import pickle
 
 from sklearn.compose import make_column_transformer
-from sklearn.dummy import DummyClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import make_scorer, f1_score, SCORERS
-from sklearn.model_selection import (
-    RandomizedSearchCV,
-    cross_validate,
-    train_test_split,
-)
+from sklearn.metrics import f1_score, SCORERS, classification_report
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import (
     OneHotEncoder,
     OrdinalEncoder,
 )
-from sklearn.svm import SVC
-from sklearn.naive_bayes import MultinomialNB
 
 opt = docopt(__doc__)
 
-def main(test, out_dir)
+def main(test, out_dir):
+    """
+    Assess best model's accuracy on test data set.
+    Report classification report of weighted_f1 score.
+    
+    Parameters
+    ----------
+    test: str
+        file path and file name of the test data set
+    out_dir: str
+        file path and file name of the model_quality report
+    """
 
     # Load and wrangle test data
     test_data = pd.read_csv(test)
@@ -49,10 +52,16 @@ def main(test, out_dir)
     y_predict = final_model.predict(X_test)
     
     # Assess model accuracy
-    model_quality = f1_score(y_test, y_predict, sample_weight=())
+    model_quality = pd.DataFrame(classification_report(y_test, y_predict, output_dict=True)).T
+    
+    # Save model_quality to results
+    try:
+        model_quality.to_csv(out_dir, index=False)
+    except:
+        os.makedirs(os.path.dirname(out_dir))
+        model_quality.to_csv(out_dir, index=False)
+    
 
 if __name__ == "__main__":
     main(opt["--"])
     
-    
-    import pickle
